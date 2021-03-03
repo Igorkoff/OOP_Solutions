eddc4099d4d855012b66a5ea3b63e814a5b3dc6a
Igor Alekhnovych <igoraleh@gmail.com>
2021-03-03 18:05:04 +0000

    Gant Bart Chart - 75% Done

--- java/src/ie/tudublin/Gant.java
+++ java/src/ie/tudublin/Gant.java
@@ -1,23 +1,43 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
 
 public class Gant extends PApplet
-{	
-	
-	public void settings()
-	{
-		size(800, 600);
-	}
+{
+    ArrayList<Task> tasks = new ArrayList<Task>();  // create ArrayList
+
+    private boolean isEnd = false;
+    private int whichTask = -1;
+    private int maxMonths = 30;
+
+    float border = 0;
+    float border_left = 0;
+    float rowHeight = 0;
 
 	public void loadTasks()
 	{
-		
-	}
+        Table table = loadTable("tasks.csv", "header");  // create Table
+
+        for (TableRow row:table.rows())
+        {
+            Task t = new Task(row);     // create star
+            tasks.add(t);               // add star to the array
+
+        }   // end for
+
+	}   // end loadTasks
 
 	public void printTasks()
 	{
-		
+		for (Task t: tasks)
+        {
+            println(t);
+
+        }   // end for
 	}
 	
 	public void mousePressed()
@@ -30,14 +50,72 @@ public class Gant extends PApplet
 		println("Mouse dragged");
 	}
 
-	
+    public void settings()
+	{
+        fullScreen();
+
+	}   // end settings
 	
 	public void setup() 
 	{
-	}
+        textAlign(CENTER, CENTER);
+        colorMode(HSB);
+        textSize(14);
+
+        border = 0.1f * height;
+        border_left = 0.2f * width;
+        rowHeight = 0.05f * height;
+
+        loadTasks();
+        printTasks();
+
+	}   // end setup
+
+    public void drawGrid()
+    {
+        for (int i = 1 ; i <= maxMonths ; i++)
+        {
+            float x = map(i, 1, maxMonths, border_left, width - border);
+
+            fill(0, 0, 100);
+            stroke(0, 0, 100);
+            line(x, border, x, height - border);    // draw vertical lines
+            text(i, x, border * 0.5f);              // draw numbers on top
+
+        }   // end for
+
+    }   // end drawGrid
+
+    public void drawTasks()
+    {
+        for (int i = 0; i < tasks.size(); i++)
+        {
+            float c = map(i, 0, tasks.size(), 0, 255);
+            float x1 = map(tasks.get(i).getStart(), 1, maxMonths, border_left, width - border);
+			float x2 = map(tasks.get(i).getEnd(), 1, maxMonths, border_left, width - border);
+            float y = border + border + rowHeight * i;
+
+            // draw text
+
+            fill(0, 0, 100);
+            text(tasks.get(i).getTask(), border_left * 0.5f, y);
+
+            // draw rectangles
+
+            noStroke();
+            fill(c, 255, 255);
+			rect(x1, y - 15, x2 - x1, rowHeight, 5);
+
+        }   // end for
+
+    }   // end drawTasks
 	
 	public void draw()
 	{			
 		background(0);
-	}
-}
+        drawGrid();
+        drawTasks();
+
+	}   // end draw
+
+}   // end Gant
---
+++ java/src/ie/tudublin/Task.java
@@ -0,0 +1,73 @@
+package ie.tudublin;
+
+import processing.data.TableRow;
+
+public class Task 
+{
+    private String task;
+    private int start;
+    private int end;
+
+    // Class Constructors
+
+    public Task(TableRow row)
+    {
+        this
+        (
+            row.getString("Task"),
+            row.getInt("Start"),
+            row.getInt("End")
+        );
+
+    }   // end constructor 2
+
+    public Task(String task, int start, int end) 
+    {
+        this.task = task;
+        this.start = start;
+        this.end = end;
+
+    }   // end constructor 1
+
+    // Getters and Setters
+
+    public String getTask()
+    {
+        return task;
+    }
+
+    public void setTask(String task)
+    {
+        this.task = task;
+    }
+
+    public int getStart()
+    {
+        return start;
+    }
+
+    public void setStart(int start)
+    {
+        this.start = start;
+    }
+
+    public int getEnd() 
+    {
+		return end;
+	}
+
+	public void setEnd(int end)
+    {
+		this.end = end;
+	}
+
+    // To String
+
+    @Override
+    public String toString() 
+    {
+        return "Task [Task=" + task + ", Start=" + start + ", End=" + end + "]";
+
+    }   // end toString
+
+}   // end Task
